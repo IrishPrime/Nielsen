a274544819c8d67d88b62b953033458292ecc34c
Michael O'Neill <irish.dot@gmail.com>
2016-03-25 00:17:51 -0400

    Add README and enough code to do something useful
    - Add logging.
    - Load XDG config file.
    - Accept command line options and arguments.
    - Override config file arguments if command line options arguments are
      provided.
    - Chown files.
    - Chmod files.
    - Rename files.

---
+++ nielsen.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+"""
+chown, chmod, rename, and organize TV show files.
+"""
+import argparse
+import configparser
+import logging
+import re
+import xdg.BaseDirectory
+from os import chmod, rename
+from shutil import chown
+
+
+CONFIG = configparser.ConfigParser()
+
+
+def load_config():
+	"""Check XDG directories for configuration files."""
+	first_config = xdg.BaseDirectory.load_first_config("nielsen/nielsen.ini")
+
+	try:
+		CONFIG.read(first_config)
+	except:
+		logging.error("Failed to load config.")
+
+
+def rename_file(path):
+	"""Remove cruft from filenames.
+	Typical filenames are something like:
+		The.Glades.S02E01.Family.Matters.HDTV.XviD-FQM.avi
+	We want:
+		The Glades -02.01- Family Matters.avi
+	"""
+	logging.info("Processing %s" % path)
+	p = re.compile(r"(?P<series>.*) S(?P<season>\d+)E(?P<episode>\d+) (?P<title>.*?)?(?P<junk> HDTV.*?)? ?(?P<extension>\S{3,4})$")
+	m = p.match(re.compile("\.").sub(" ", path))
+	if m:
+		clean = "{0} -{1}.{2}- {3}.{4}".format(
+			m.group("series"),
+			m.group("season"),
+			m.group("episode"),
+			m.group("title"),
+			m.group("extension"))
+		logging.info("Moving to: {0}".format(clean))
+		rename(path, clean)
+	else:
+		logging.info("{0} did not match pattern, skipping.".format(path))
+
+
+def process_file(path):
+	"""Set ownership and permissions for files, then rename."""
+	chown(path, CONFIG['DEFAULT']['User'], CONFIG['DEFAULT']['Group'])
+	chmod(path, int(CONFIG['DEFAULT']['Mode'], 8))
+	rename_file(path)
+
+
+def main():
+	# Command line arguments
+	PARSER = argparse.ArgumentParser(description=
+		"Process episodes of TV shows for storage on a media server.")
+	PARSER.add_argument("-u", "--user", dest="user", help="User to own files")
+	PARSER.add_argument("-g", "--group", dest="group", help="Group to own files")
+	PARSER.add_argument("-m", "--mode", dest="mode", type=str,
+		help="File mode (permissions) in octal")
+	PARSER.add_argument("-l", "--log", dest="log_level", type=str,
+		default="WARNING", choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+		help="Logging level")
+	PARSER.add_argument("files", nargs="+", type=str, help="Files to operate on")
+	ARGS = PARSER.parse_args()
+
+	# Load XDG configuration file
+	load_config()
+
+	# Override the settings in the config file if given on the command line
+	if ARGS.user:
+		CONFIG['DEFAULT']['User'] = ARGS.user
+
+	if ARGS.group:
+		CONFIG['DEFAULT']['Group'] = ARGS.group
+
+	if ARGS.mode:
+		CONFIG['DEFAULT']['Mode'] = ARGS.mode
+
+	if ARGS.log_level:
+		CONFIG['DEFAULT']['LogLevel'] = ARGS.log_level.upper()
+
+	# Validate the log level
+	logging.basicConfig(filename="nielsen.log",
+		level=getattr(logging, CONFIG['DEFAULT']['LogLevel'], 30))
+
+	logging.debug("User: {0}, Group: {1}, Mode: {2}".format(
+		CONFIG['DEFAULT']['User'],
+		CONFIG['DEFAULT']['Group'],
+		CONFIG['DEFAULT']['Mode']))
+
+	# Iterate over files
+	for f in ARGS.files:
+		logging.info(f)
+		process_file(f)
+
+
+if __name__ == "__main__":
+	main()
+
+# vim: tabstop=4 softtabstop=4 shiftwidth=4 noexpandtab
