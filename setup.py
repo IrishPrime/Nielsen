010a38448b8c46b05fa1a20f12519f92c6f5dc10
Michael O'Neill <irish.dot@gmail.com>
2016-12-11 15:49:55 -0500

    Add Travis config and setup files

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Setup for Nielsen."""
+
+from distutils.core import setup
+
+setup(
+	name='Nielsen',
+	version='0.7.1',
+	author="Michael 'Irish' O'Neill",
+	author_email="irish.dot@gmail.com",
+	url='https://github.com/IrishPrime/nielsen/',
+	description='Rename and organize TV show files.',
+	long_description='Rename, chown, chmod, and organize TV show files from \
+		the command line.',
+	classifiers=[
+		'Development Status :: 5 - Production/Stable',
+		'Environment :: Console',
+		'Intended Audience :: Developers',
+		'Intended Audience :: End Users/Desktop',
+		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+		'Operating System :: Microsoft :: Windows',
+		'Operating System :: POSIX :: Linux',
+		'Programming Language :: Python',
+		'Topic :: Desktop Environment :: File Managers',
+		'Topic :: Multimedia :: Video',
+	],
+	platforms='linux',
+	license='GNU General Public License v3 (GPLv3)',
+	py_modules=['nielsen'],
+)
+
+# vim: tabstop=4 softtabstop=4 shiftwidth=4 noexpandtab
