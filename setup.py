24a920a04c73244aa1e9618b8fb91f7674b09ab4
Michael O'Neill <IrishPrime@users.noreply.github.com>
2016-12-20 09:56:00 -0500

    Prevent FileNotFoundError (#45)
    - Check that file exists on any argument given to `process_file`.
    - Bump version.
    - Resolves #43.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
 	name='Nielsen',
-	version='0.9.4',
+	version='0.9.5',
 	author="Michael 'Irish' O'Neill",
 	author_email="irish.dot@gmail.com",
 	url='https://github.com/IrishPrime/nielsen/',
