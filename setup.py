8d9b366ec7c9fbc3afd785fc2fbac9eb2b9a148f
Michael O'Neill <irish.dot@gmail.com>
2016-12-24 17:46:49 -0500

    Fix typo in classifiers

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ setup(
 		'Operating System :: Microsoft :: Windows',
 		'Operating System :: POSIX :: Linux',
 		'Programming Language :: Python :: 3.3',
-		'Programming Language :: Python :: 3.4'
-		'Programming Language :: Python :: 3.5'
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
 		'Programming Language :: Python',
 		'Topic :: Desktop Environment :: File Managers',
 		'Topic :: Multimedia :: Video',
