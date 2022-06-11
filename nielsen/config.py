5cc77c39b93d2f3279c2b4af90a220ad925a4145
Michael "Irish" O'Neill <irish.dot@gmail.com>
2022-06-11 15:33:44 -0400

    Add support for reading configuration files
    Add `vim` modelines

--- nielsen/config.py
+++ nielsen/config.py
@@ -1,5 +1,31 @@
 """Interact with configuration files and objects."""
 
 from configparser import ConfigParser
+from typing import Optional
+import pathlib
 
 config: ConfigParser = ConfigParser()
+
+
+def load_config(path: Optional[pathlib.Path] = None) -> ConfigParser:
+    """Load a configuration from a file. If no file path is provided, default
+    configuration file locations are used. Returns a ConfigParser object."""
+
+    if not path:
+        config.read(
+            [
+                "/etc/nielsen/config.ini",
+                pathlib.Path("~/.nielsen/config.ini").expanduser(),
+            ]
+        )
+    else:
+        config.read(path)
+
+    return config
+
+
+def write_config(path: pathlib.Path) -> None:
+    """Write the global configuration object to the given `path`."""
+
+
+# vim: et ts=4 sts=4 sw=4
