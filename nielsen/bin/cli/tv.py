969e2239d5e7c26e35a11a396c039b557cc5c6f6
Michael "Irish" O'Neill <irish@lasso.io>
2024-08-12 16:36:01 -0400

    WIP Add `nielsen tv info` command

--- nielsen/bin/cli/tv.py
+++ nielsen/bin/cli/tv.py
@@ -3,6 +3,7 @@
 
 import logging
 from pathlib import Path
+from typing import Any, Optional
 from typing_extensions import Annotated
 
 import nielsen.config
@@ -11,6 +12,7 @@ import nielsen.media
 from nielsen.config import config as config
 
 import typer
+
 from rich.pretty import pprint
 
 
@@ -21,17 +23,55 @@ app: typer.Typer = typer.Typer(name="TV")
 
 
 @app.command()
-def series(
-    interactive: Annotated[bool, typer.Option()],
-    series: Annotated[list[str], typer.Argument(help="Show title to search for")],
+def info(
+    series: Annotated[
+        Optional[str], typer.Option(help="Show title to search for")
+    ] = None,
+    series_id: Annotated[
+        Optional[int], typer.Option(help="The TVMaze series ID")
+    ] = None,
+    season: Annotated[
+        Optional[int],
+        typer.Option(help="Season number (to get additional details)"),
+    ] = None,
+    episode: Annotated[
+        Optional[int],
+        typer.Option(help="Episode number (to get additional details)"),
+    ] = None,
+    interactive: Annotated[
+        bool,
+        typer.Option(
+            is_flag=True,
+            help="Interactively select from multiple results",
+        ),
+    ] = True,
 ) -> None:
     """Search for a TV series by title."""
 
-    flattened: str = " ".join(series)
-    media: nielsen.media.TV = nielsen.media.TV(Path("/dev/null"), series=flattened)
+    if series and series_id:
+        typer.echo("The --series and --series-id options are mutually exclusive.")
+        raise typer.Exit(1)
+    elif not (series or series_id):
+        typer.echo("One of --series or --series-id required.")
+        raise typer.Exit(2)
+
+    if episode and not season:
+        typer.echo("Cannot fetch episode information without a season.")
+        raise typer.Exit(3)
+
     fetcher: nielsen.fetcher.TVMaze = nielsen.fetcher.TVMaze()
 
-    fetcher.get_series_id_search(media)
+    if series:
+        series_id = fetcher.get_series_id(series, interactive)
+
+    if series_id and season and episode:
+        typer.echo("Get additional information for the episode")
+
+    elif series_id and season:
+        typer.echo("Get additional information for the season")
+
+    elif series_id:
+        typer.echo(f"{series_id=}")
 
 
 if __name__ == "__main__":
--- nielsen/fetcher.py
+++ nielsen/fetcher.py
@@ -1,16 +1,12 @@
 """Fetchers are used to query remote sources for metadata about a given Media object."""
 
-from typing import Any
-from typing import Optional
-from typing import Protocol
-from typing import TypeVar
 import logging
 import urllib.parse
+from typing import Any, Optional, Protocol, TypeVar
 
 import requests
 
 import nielsen.media
-
 from nielsen.config import config
 
 logger: logging.Logger = logging.getLogger(__name__)
@@ -40,7 +36,7 @@ class TVMaze:
         record the series ID in the config."""
 
         series_id: Optional[int] = self.get_series_id(
-            media, config.getboolean("DEFAULT", "interactive")
+            media.series, config.getboolean("DEFAULT", "interactive")
         )
         if series_id:
             self.set_series_id(media.series, str(series_id))
@@ -58,41 +54,41 @@ class TVMaze:
 
         config.set(self.IDS, series, str(id))
 
-    def get_series_id(self, media: nielsen.media.TV, interactive: bool = False) -> int:
+    def get_series_id(self, series: str, interactive: bool = False) -> int:
         """Return the TVMaze ID for the series. Will check for a local config file first
         and search TVMaze if a local match isn't found. Optionally, prompt the user to
         select the correct series interactively if multiple results are found."""
 
-        if config.has_option(self.IDS, media.series):
+        if config.has_option(self.IDS, series):
             lookup = self.get_series_id_local
         elif interactive:
             lookup = self.get_series_id_search
         else:
             lookup = self.get_series_id_singlesearch
 
-        series_id: int = lookup(media)
-        logger.debug("Series: %s, ID: %s", media.series, series_id)
+        series_id: int = lookup(series)
+        logger.debug("Series: %s, ID: %s", series, series_id)
 
         return series_id
 
-    def get_series_id_local(self, media: nielsen.media.TV) -> int:
+    def get_series_id_local(self, series: str) -> int:
         """Get the series ID from the configuration."""
 
-        return config.getint(self.IDS, media.series, fallback=0)
+        return config.getint(self.IDS, series, fallback=0)
 
-    def get_series_id_search(self, media: nielsen.media.TV) -> int:
+    def get_series_id_search(self, series: str) -> int:
         """Get the series ID from the TVMaze `search` endpoint, which can return
         multiple results. If multiple results are returned, prompt the user to pick one.
         URL: /search/shows?q=:query.
         """
 
         request: str = (
-            f"{self.SERVICE}/search/shows/?q={urllib.parse.quote_plus(media.series)}"
+            f"{self.SERVICE}/search/shows/?q={urllib.parse.quote_plus(series)}"
         )
         response: requests.Response = requests.get(request)
         rjson: dict[Any, Any] = response.json()
 
-        logging.debug("Media: %r\nRequest: %r\nResponse: %s", media, request, rjson)
+        logging.debug("Series: %s\nRequest: %r\nResponse: %s", series, request, rjson)
 
         series_id: int = 0
         # If TVMaze returns an empty list, return 0.
@@ -107,30 +103,67 @@ class TVMaze:
         # TODO: The nature of the picker should be left up to the frontend
         # implementation, but a console prompt is sufficient for now since the console
         # frontend will be the first (and maybe only) implementation.
-        print(f"Search results for: {media.series} ({media.path})")
-        for option, result in enumerate(rjson):
+        print(f"Search results for: {series}")
+        for option, result in enumerate(rjson, start=1):
             name: str = result["show"]["name"]
-            series_id = result["show"]["id"]
+            series_id: int = result["show"]["id"]
             premiered: int = result["show"]["premiered"]
-            print(f"{option}. {name} (ID: {series_id}, Premiered: {premiered})")
 
+            # All these ifs and try/except blocks look silly, but it's an order of
+            # magnitude faster than reducing or using defaultdict
+            if result["show"]["network"]:
+                try:
+                    network: str = result["show"]["network"].get("name", "Unknown")
+                except (AttributeError, KeyError, TypeError):
+                    network: str = "Unknown"
+
+                try:
+                    country: str = result["show"]["network"]["country"].get(
+                        "name", "Unknown"
+                    )
+                except (AttributeError, KeyError, TypeError):
+                    country: str = "Unknown"
+
+            elif result["show"]["webChannel"]:
+                try:
+                    network: str = result["show"]["webChannel"].get("name", "Unknown")
+                except (AttributeError, KeyError, TypeError):
+                    network: str = "Unknown"
+
+                try:
+                    country: str = result["show"]["webChannel"]["country"].get(
+                        "name", "Unknown"
+                    )
+                except (AttributeError, KeyError, TypeError):
+                    country: str = "Unknown"
+
+            else:
+                network: str = "Unknown"
+                country: str = "Unknown"
+
+            print(
+                f"{option}. {name} (Premiered: {premiered}, Network: {network}, Country: {country}, ID: {series_id})"
+            )
         # TODO: Handle invalid selections.
-        selection: int = int(input("Select series: "))
+        selection: int = int(input("Select series: ")) - 1
 
         # Add selected ID to config and return it.
         series_id = rjson[selection]["show"]["id"]
+
         return series_id
 
-    def get_series_id_singlesearch(self, media: nielsen.media.TV) -> int:
+    def get_series_id_singlesearch(self, series: str) -> int:
         """Get the series ID from the TVMaze `singlesearch` endpoint, which returns a
         single result that it considers the best match.
         URL: /singlesearch/shows?q=:query
         """
 
-        request: str = f"{self.SERVICE}/singlesearch/shows/?q={urllib.parse.quote_plus(media.series)}"
+        request: str = (
+            f"{self.SERVICE}/singlesearch/shows/?q={urllib.parse.quote_plus(series)}"
+        )
         response: requests.Response = requests.get(request)
         rjson: dict[Any, Any] = response.json()
-        logger.debug("Media: %r\nRequest: %r\nResponse: %s", media, request, rjson)
+        logger.debug("Media: %r\nRequest: %r\nResponse: %s", series, request, rjson)
 
         if response.ok:
             return rjson.get("id", 0)
@@ -154,7 +187,9 @@ class TVMaze:
         if not media.episode:
             raise ValueError("No Episode Number")
 
-        request: str = f"{self.SERVICE}/shows/{series_id}/episodebynumber?season={media.season}&number={media.episode}"
+        request: str = (
+            f"{self.SERVICE}/shows/{series_id}/episodebynumber?season={media.season}&number={media.episode}"
+        )
         response: requests.Response = requests.get(request)
         rjson: dict[Any, Any] = response.json()
         logging.debug("Media: %r\nRequest: %r\nResponse: %s", media, request, rjson)
@@ -163,6 +198,65 @@ class TVMaze:
             episode_title = str(rjson.get("name"))
 
         return episode_title
+
+    def search_series(self, series: str, interactive: bool = True) -> dict[str, Any]:
+        """Return a dictionary of information about a series."""
+
+        request: str = (
+            f"{self.SERVICE}/search/shows/?q={urllib.parse.quote_plus(series)}"
+        )
+
+        response: requests.Response = requests.get(request)
+        rjson: dict[Any, Any] = response.json()
+
+        if len(rjson) == 1 or not interactive:
+            return rjson[0]["show"]
+
+        for option, result in enumerate(rjson, start=1):
+            name: str = result["show"]["name"]
+            series_id: int = result["show"]["id"]
+            premiered: int = result["show"]["premiered"]
+
+            # All these ifs and try/except blocks look silly, but it's an order of
+            # magnitude faster than reducing or using defaultdict
+            if result["show"]["network"]:
+                try:
+                    network: str = result["show"]["network"].get("name", "Unknown")
+                except (AttributeError, KeyError, TypeError):
+                    network: str = "Unknown"
+
+                try:
+                    country: str = result["show"]["network"]["country"].get(
+                        "name", "Unknown"
+                    )
+                except (AttributeError, KeyError, TypeError):
+                    country: str = "Unknown"
+
+            elif result["show"]["webChannel"]:
+                try:
+                    network: str = result["show"]["webChannel"].get("name", "Unknown")
+                except (AttributeError, KeyError, TypeError):
+                    network: str = "Unknown"
+
+                try:
+                    country: str = result["show"]["webChannel"]["country"].get(
+                        "name", "Unknown"
+                    )
+                except (AttributeError, KeyError, TypeError):
+                    country: str = "Unknown"
+
+            else:
+                network: str = "Unknown"
+                country: str = "Unknown"
+
+            print(
+                f"{option}. {name} (Premiered: {premiered}, Network: {network}, Country: {country}, ID: {series_id})"
+            )
+
+        # TODO: Handle invalid selections.
+        selection: int = int(input("Select series: "))
+
+        return rjson[selection - 1]["show"]
 
 
 # vim: tabstop=4 softtabstop=4 shiftwidth=4 expandtab textwidth=88
